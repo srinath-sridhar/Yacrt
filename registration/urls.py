3846424ff76ce8155198975a90d6eb1cf52f85d2
srinath-sridhar <srinath.sridhar.89@gmail.com>
2013-02-21 17:20:22 -0500

    updated urls to include login_success

--- registration/urls.py
+++ registration/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from registration import views
 
 urlpatterns = patterns('',
-                       url(r'^signin/$',views.signin_form ),
-                       url(r'^authenticate_user/',views.authenticate_user),
+                       url(r'^signin/$',views.signin_form, name="signin_form"),
+                       url(r'^authenticate_user/',views.authenticate_user, name="authenticate_user"),
+                       url(r'^home/',views.login_success, name="login_success"),
                        )
