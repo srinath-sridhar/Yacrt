8fea321e77d8a2984d44d505dc3fe9898e4a0b4f
Srinath <srinath@ubuntu-dev-vm.(none)>
2013-02-24 02:34:27 -0500

    Initial repo browser page

--- repobrowser/models.py
+++ repobrowser/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
-# Create your models here.
+
+class Repository(models.Model):
+    repo_name = models.CharField(max_length=100)
+    repo_description = models.CharField(max_length=200)
+    repo_url = models.CharField(max_length=200)
+    repo_access_group = models.ForeignKey(Group)
+    repo_vc_system = models.CharField(max_length=15)
+    
+    
--- repobrowser/views.py
+++ repobrowser/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render_to_response
 from django.template import RequestContext
-from django.contrib.auth import authenticate, login
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect, render
 from django.utils.html import strip_tags
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from repobrowser.models import Repository
+
 
 # home page of the user on successful login
 # displays all the repositories that user has access to
@@ -13,4 +13,11 @@ from django.contrib.auth.decorators import login_required
 def repos_home(request):
     user_name = request.session.get('name')
     user_name = strip_tags(user_name)
-    return render(request, "repobrowser/repos_home.html", {'user_name':user_name})
+    repos = get_repos(request.user)
+    return render(request, "repobrowser/repos_home.html", {'user_name':user_name, 'repos':repos})
+
+# retireves all repos the user has access to
+def get_repos(user):
+    repos = Repository.objects.filter(repo_access_group__in=user.groups.all()) 
+    return repos
+    
